1954b8eee6373d7fec331d94051518632082e88a
TOTSILA Dionis <dionisistotsilas@gmail.com>
2026-08-28 23:49:18 +0200

    weight instead of lambda

--- src/tiago_control_node/tiago_control_node/tiago_pro_opensot_node.py
+++ src/tiago_control_node/tiago_control_node/tiago_pro_opensot_node.py
@@ -502,10 +502,11 @@ def main(args=None):
 
             # Gaze Task
             if node.gaze_locked:
-                tasks["gaze"].setLambda(0.0)
+                tasks["gaze"].setWeight(0.0)
+                # tasks["gaze"].setLambda(0.0)
             else:
                 T = model.getPose("gripper_right_grasping_link", "base_link")
-                tasks["gaze"].setLambda(0.025)
+                tasks["gaze"].setWeight(1.0)
 
             # NOTE: Duplicate Cartesian command block removed from here!
 
